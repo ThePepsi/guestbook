283460736684f389af9c4bac2de26b7b5bfd7ecf
ThePepsi <timo.seitz96@outlook.com>
2019-10-19 17:33:16 +0200

    Added eMail support

--- src/main/java/guestbook/GuestbookEntry.java
+++ src/main/java/guestbook/GuestbookEntry.java
@@ -36,6 +36,7 @@ class GuestbookEntry {
 	private @Id @GeneratedValue Long id;
 	private final String name, text;
 	private final LocalDateTime date;
+	private final String email;
 
 	/**
 	 * Creates a new {@link GuestbookEntry} for the given name and text.
@@ -43,6 +44,19 @@ class GuestbookEntry {
 	 * @param name must not be {@literal null} or empty
 	 * @param text must not be {@literal null} or empty
 	 */
+	public GuestbookEntry(String name, String text, String email) {
+
+		Assert.hasText(name, "Name must not be null or empty!");
+		Assert.hasText(text, "Text must not be null or empty!");
+		Assert.hasText(email, "eMail must not be null or empty!");
+
+		this.name = name;
+		this.text = text;
+		this.date = LocalDateTime.now();
+		this.email = email;
+	}
+
+	@SuppressWarnings("unused")
 	public GuestbookEntry(String name, String text) {
 
 		Assert.hasText(name, "Name must not be null or empty!");
@@ -51,10 +65,10 @@ class GuestbookEntry {
 		this.name = name;
 		this.text = text;
 		this.date = LocalDateTime.now();
+		this.email = "tester@mail.com";
 	}
-
-	@SuppressWarnings("unused")
 	private GuestbookEntry() {
+		this.email = null;
 		this.name = null;
 		this.text = null;
 		this.date = null;
@@ -75,4 +89,6 @@ class GuestbookEntry {
 	public String getText() {
 		return text;
 	}
+
+	public String getEmail(){ return email;}
 }
